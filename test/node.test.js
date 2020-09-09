2e9ffe8c0e64403f241f28f21f2b268ccf917ab2
Linda <fernsel@htw-berlin.de>
2020-09-09 19:39:41 +0200

    improved test structure for linked list

--- test/node.test.js
+++ test/node.test.js
@@ -1,22 +1,28 @@
 const Node = require("../lib/node");
 
-test("there can be lists out of one node", () => {
-	const head = new Node(1);
-	expect(head.toString()).toStrictEqual("1");
+describe("create list", () => {
+	test("there can be lists out of one node", () => {
+		const head = new Node(1);
+		expect(head.toString()).toStrictEqual("1");
+	});
+
+	test("there can be lists out of several nodes", () => {
+		const next = new Node(2);
+		const head = new Node(1, next);
+		expect(head.toString()).toStrictEqual("1, 2");
+	});
+
+	test("a list can be created from comma separated list (string)", () => {
+		const list = Node.fromString("1, 2");
+		expect(list.toString()).toStrictEqual("1, 2");
+		expect(list.next.toString()).toStrictEqual("2");
+	});
 });
 
-test("there can be lists out of several nodes", () => {
-	const next = new Node(2);
-	const head = new Node(1, next);
-	expect(head.toString()).toStrictEqual("1, 2");
+describe("delete node from list", () => {
+	// Add tests for the delete method
 });
 
-test("a list can be created from comma separated list (string)", () => {
-	const list = Node.fromString("1, 2");
-	expect(list.toString()).toStrictEqual("1, 2");
-	expect(list.next.toString()).toStrictEqual("2");
+describe("reverse list", () => {
+	// Add tests for the reverse method
 });
-
-// Add tests for the delete method
-
-// Add tests for the reverse method
