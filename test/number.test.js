693b6e17b2c2c4b2c51162b499ecf9340824ea9b
erirapce <s0566187@htw-berlin.de>
2020-12-09 00:39:08 +0100

    add test cases for number

--- test/number.test.js
+++ test/number.test.js
@@ -1,1 +1,12 @@
 // Implement your test cases for number.js here.
+
+const Number = require("../lib/number");
+
+
+  test("absolute value of 1 is 1", () => {
+    expect(Number.getAbsolute(1)) == 1;
+  });
+
+  test("absolute value of -1 is 1", () => {
+    expect(Number.getAbsolute(-1)) == 1;
+  });
