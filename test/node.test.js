bcbff3e02229da7b3d8579e126de154cc481c9c8
erirapce <s0566187@htw-berlin.de>
2020-12-09 00:53:23 +0100

    implement the delete and reverse method and their test cases

--- test/node.test.js
+++ test/node.test.js
@@ -21,8 +21,26 @@ describe("create list", () => {
 
 describe("delete node from list", () => {
 	// Add tests for the delete method
+	test("return the list without the deleted node", () => {
+		const node1 = new Node(1);
+		const node2 = new Node(1,2);
+		const node3 = new Node(1,3);
+		let list = [node1, node2, node3];
+		const head = new Node();
+		expect(head.delete(list, node1).toString()).toStrictEqual("1, 2,1, 3");
+});
 });
 
 describe("reverse list", () => {
 	// Add tests for the reverse method
+	test("return the reversed list", () => {
+		const node1 = new Node(1,1);
+		const node2 = new Node(1,2);
+		const node3 = new Node(1,3);
+		let list = [node1, node2, node3];
+		const head = new Node();
+		//as we understood, we need to reverse the nodes
+		//so revList should be [node3, node2, node1]
+		expect(head.reverse(list).toString()).toStrictEqual("1, 3,1, 2,1, 1")
+	})
 });
